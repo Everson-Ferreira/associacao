5bdf9127c7c8c299315be154d651a4f29892ad86
Everson-Fagundes <eversonffagundes@gmail.com>
2020-11-25 21:56:14 -0300

    arrumando herança de salvar, pesquisar por nome, e pesquisar por id

--- src/br/com/associacao/dao/ClienteDaoImpl.java
+++ src/br/com/associacao/dao/ClienteDaoImpl.java
@@ -28,9 +28,9 @@ public class ClienteDaoImpl extends PessoaDaoImpl implements Serializable {
         super.salvar(pessoa);
         Cliente cliente = (Cliente) pessoa;  //casting
         String sql = "INSERT INTO cliente(salario, idPessoa)"
-                                           + " VALUES(?, ?)";
+                + " VALUES(?, ?)";
         try {
-            preparando = conexao.prepareStatement(sql); 
+            preparando = conexao.prepareStatement(sql);
             preparando.setDouble(1, cliente.getSalario());
             preparando.setInt(2, cliente.getId());
             preparando.executeUpdate();
@@ -40,6 +40,18 @@ public class ClienteDaoImpl extends PessoaDaoImpl implements Serializable {
             System.err.println("Erro ao salvar cliente " + eSQL.getMessage());
         } finally {
             FabricaConexao.fecharConexao(conexao, preparando, resultSet);
+        }
+    }
+     public void excluir(Integer id) throws SQLException {
+        try {
+            conexao = FabricaConexao.abrirConexao();
+            preparando = conexao.prepareStatement("DELETE FROM pessoa WHERE id = ?");
+            preparando.setInt(1, id);
+            preparando.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("Erro ao excluir " + e.getMessage());
+        } finally {
+            FabricaConexao.fecharConexao(conexao, preparando);
         }
     }
 
@@ -55,7 +67,7 @@ public class ClienteDaoImpl extends PessoaDaoImpl implements Serializable {
             preparando.setDouble(4, cliente.getSalario());
             preparando.setInt(5, cliente.getId());
             preparando.executeUpdate();
-            
+
             EnderecoDaoImpl enderecoDaoImpl = new EnderecoDaoImpl();
             enderecoDaoImpl.alterarEndereco(cliente.getEndereco(), conexao);
 
@@ -67,24 +79,14 @@ public class ClienteDaoImpl extends PessoaDaoImpl implements Serializable {
         }
     }
 
-    public void excluir(Integer id) throws SQLException {
-        try {
-            conexao = FabricaConexao.abrirConexao();
-            preparando = conexao.prepareStatement("DELETE FROM cliente WHERE id = ?");
-            preparando.setInt(1, id);
-            preparando.executeUpdate();
-        } catch (SQLException e) {
-            System.err.println("Erro ao excluir " + e.getMessage());
-        } finally {
-            FabricaConexao.fecharConexao(conexao, preparando);
-        }
-    }
+    
 
     public Cliente pesquisarPorId(Integer id) throws SQLException {
         Cliente cliente = null;
-        String consulta = "SELECT * FROM cliente c"
-                + " JOIN endereco e on e.idCliente = c.id"
-                + " WHERE c.id = ?";
+        String consulta = "SELECT * FROM cliente c "
+                + "inner join pessoa p on c.idPessoa = p.id "
+                + "inner JOIN endereco e on e.idCliente = c.idPessoa  "
+                + "WHERE c.idPessoa = ?";
 
         try {
             conexao = FabricaConexao.abrirConexao();
@@ -118,22 +120,36 @@ public class ClienteDaoImpl extends PessoaDaoImpl implements Serializable {
     }
 
     public List<Cliente> pesquisarPorNome(String nome) throws SQLException {
-        String consulta = "SELECT * FROM cliente  WHERE nome LIKE ?";
+        String consulta = "SELECT * FROM cliente c "
+                + "inner join pessoa p on c.idPessoa = p.id "
+                + "inner JOIN endereco e on e.idCliente = c.idPessoa  "
+                + "WHERE p.nome like ?";
+
         Cliente cliente;
+    
         List<Cliente> clientes = new ArrayList<>();
         try {
-            System.out.println("Teste do GitHub");
+            
             conexao = FabricaConexao.abrirConexao();
             preparando = conexao.prepareStatement(consulta);
             preparando.setString(1, "%" + nome + "%");
             resultSet = preparando.executeQuery();
             while (resultSet.next()) {
                 cliente = new Cliente();
-                cliente.setId(resultSet.getInt("id"));
+                cliente.setId(resultSet.getInt("p.id"));
                 cliente.setNome(resultSet.getString("nome"));
                 cliente.setEmail(resultSet.getString("email"));
                 cliente.setTelefone(resultSet.getString("telefone"));
                 cliente.setSalario(resultSet.getDouble("salario"));
+                cliente.setEndereco(new Endereco());
+                cliente.getEndereco().setId(resultSet.getInt("e.id"));
+                cliente.getEndereco().setLogradouro(resultSet.getString("logradouro"));
+                cliente.getEndereco().setNumero(resultSet.getString("numero"));
+                cliente.getEndereco().setBairro(resultSet.getString("bairro"));
+                cliente.getEndereco().setCidade(resultSet.getString("cidade"));
+                cliente.getEndereco().setEstado(resultSet.getString("estado"));
+                cliente.getEndereco().setCep(resultSet.getString("cep"));
+               
                 clientes.add(cliente);
             }
         } catch (SQLException e) {
@@ -144,4 +160,5 @@ public class ClienteDaoImpl extends PessoaDaoImpl implements Serializable {
 
         return clientes;
     }
+   
 }
--- test/br/com/associacao/dao/ClienteDaoImplTest.java
+++ test/br/com/associacao/dao/ClienteDaoImplTest.java
@@ -17,66 +17,66 @@ import org.junit.Test;
  */
 public class ClienteDaoImplTest {
 
-    private Cliente cliente;
+    private Cliente cli;
     private ClienteDaoImpl clienteDaoImpl;
 
     public ClienteDaoImplTest() {
         clienteDaoImpl = new ClienteDaoImpl();
     }
 
-    @Test
+//    @Test
     public void testSalvarCliente() throws Exception {
         System.out.println("salvar");
-        cliente = new Cliente(null, UtilGerador.gerarCaracter(10), UtilGerador.gerarEmail(),
+        cli = new Cliente(null, UtilGerador.gerarCaracter(10), UtilGerador.gerarEmail(),
                 UtilGerador.gerarTelefoneFixo(), Double.parseDouble(UtilGerador.gerarNumero(3)));
 
         Endereco endereco = new Endereco(null, UtilGerador.gerarCaracter(10), UtilGerador.gerarNumero(3),
                 UtilGerador.gerarCaracter(10), UtilGerador.gerarCaracter(10), UtilGerador.gerarCaracter(2),
                 UtilGerador.gerarNumero(5) + "-" + UtilGerador.gerarNumero(3));
-        
-        cliente.setEndereco(endereco);
-        
-        clienteDaoImpl.salvar(cliente);
+
+        cli.setEndereco(endereco);
+
+        clienteDaoImpl.salvar(cli);
     }
-    
+
 //    @org.junit.Test
     public void testAlterar() throws Exception {
         System.out.println("alterar");
-        
-        cliente = clienteDaoImpl.pesquisarPorId(11);
-        cliente.setNome("aaa" + UtilGerador.gerarNome());
-        cliente.getEndereco().setLogradouro("log alterado");
-        
-        clienteDaoImpl.alterar(cliente);
+
+        cli = clienteDaoImpl.pesquisarPorId(11);
+        cli.setNome("aaa" + UtilGerador.gerarNome());
+        cli.getEndereco().setLogradouro("log alterado");
+
+        clienteDaoImpl.alterar(cli);
     }
 
-    // @Test
+     @Test
     public void testExcluir() throws Exception {
         System.out.println("excluir");
         clienteDaoImpl.excluir(3);
     }
 
-    //@Test
+//    @Test
     public void testPesquisarPorId() throws Exception {
         System.out.println("pesquisar Por Id");
-        cliente = clienteDaoImpl.pesquisarPorId(11);
-        System.out.println("id: " + cliente.getId());
-        System.out.println("nome: " + cliente.getNome());
-        System.out.println("email: " + cliente.getEmail());
-        System.out.println("telefone: " + cliente.getTelefone());
-        System.out.println("salario: R$" + cliente.getSalario());
-        System.out.println("Id Endereço: " + cliente.getEndereco().getId());
-        System.out.println("Logradouro: " + cliente.getEndereco().getLogradouro());
-        System.out.println("Número: " + cliente.getEndereco().getNumero());
-        System.out.println("Bairro: " + cliente.getEndereco().getBairro());
-        System.out.println("Estado: " + cliente.getEndereco().getEstado());
-        System.out.println("CEP: " + cliente.getEndereco().getCep());
-            }
+        cli = clienteDaoImpl.pesquisarPorId(3);
+        System.out.println("id: " + cli.getId());
+        System.out.println("nome: " + cli.getNome());
+        System.out.println("email: " + cli.getEmail());
+        System.out.println("telefone: " + cli.getTelefone());
+        System.out.println("salario: R$" + cli.getSalario());
+        System.out.println("Id Endereço: " + cli.getEndereco().getId());
+        System.out.println("Logradouro: " + cli.getEndereco().getLogradouro());
+        System.out.println("Número: " + cli.getEndereco().getNumero());
+        System.out.println("Bairro: " + cli.getEndereco().getBairro());
+        System.out.println("Estado: " + cli.getEndereco().getEstado());
+        System.out.println("CEP: " + cli.getEndereco().getCep());
+    }
 
-    //@Test
+//    @Test
     public void testPesquisarPorNome() throws Exception {
         System.out.println("pesquisar Por Nome");
-        List<Cliente> clientes = clienteDaoImpl.pesquisarPorNome("D");
+        List<Cliente> clientes = clienteDaoImpl.pesquisarPorNome("M");
         for (Cliente cli : clientes) {
             System.out.println("id: " + cli.getId());
             System.out.println("nome: " + cli.getNome());
@@ -84,6 +84,14 @@ public class ClienteDaoImplTest {
             System.out.println("telefone: " + cli.getTelefone());
             System.out.println("salario: R$" + cli.getSalario());
             System.out.println("");
+            System.out.println("Id Endereço: " + cli.getEndereco().getId());
+            System.out.println("Logradouro: " + cli.getEndereco().getLogradouro());
+            System.out.println("Número: " + cli.getEndereco().getNumero());
+            System.out.println("Bairro: " + cli.getEndereco().getBairro());
+            System.out.println("Estado: " + cli.getEndereco().getEstado());
+            System.out.println("CEP: " + cli.getEndereco().getCep());
+            System.out.println("");
         }
+
     }
 }
